668b2739a8a011b9e3a851fa0e3f58d88c034997
vonhatphuongahihi <vonhatphuongahihi@gmail.com>
2025-04-03 14:59:31 +0700

    base

---
+++ src/main/java/com/example/vibely_backend/VibelyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.vibely_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibelyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibelyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vibely_backend/VibelyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vibely_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibelyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
