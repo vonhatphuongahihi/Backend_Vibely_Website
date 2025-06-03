67bedc06dff9cbde7143fdd9e816121504fd993b
Hoanggiaminh <hoanggiaminh27012004@gmail.com>
2025-06-03 11:44:02 +0700

    fix document, saved

--- src/main/java/com/example/vibely_backend/entity/Subject.java
+++ src/main/java/com/example/vibely_backend/entity/Subject.java
@@ -6,7 +6,6 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,17 +20,13 @@ public class Subject {
     @Id
     private String id;
 
-    @Field("name")
     private String name;
 
-    @Field("level_id")
     private String levelId;
 
     @CreatedDate
-    @Field("created_at")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @Field("updated_at")
     private LocalDateTime updatedAt;
 }
